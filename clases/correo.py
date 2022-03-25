fc63722669ec486d184502f971f0abb14b901782
Barroso03 <91721590+Barroso03@users.noreply.github.com>
2022-03-25 16:07:32 +0000

    terminamos clase

---
+++ clases/correo.py
@@ -0,0 +1,26 @@
+class web():
+    def __init__(self,correo,entrar):
+        self.correo = correo
+        self.entar = entrar
+    def set_correo(self,nombre,final,medio,todo):
+        self.nombre = nombre
+        self.final = final
+        self.medio = medio
+        self.todo = todo
+        todo = (nombre,final,medio)
+        if todo == str :
+            print("{}".format(nombre), "@","{}".format(medio),"{}".format(final))
+        else:
+            print("Puede ser un ciber ataque")
+    def get_correo(self,correo):
+        self.correo = correo
+    def set_entrar(self,entrar):
+        self.entrar = entrar
+        if web.set_correo() == True:
+            print("Has entrado en la web")
+    def get_entrar(self,entrar):
+        self.entrar = entrar
+        
+print(input("Introduce tu correo:"))
+web.get_correo("correo")
+web.get_entrar("entrar")
